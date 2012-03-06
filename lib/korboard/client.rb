a3685cf8379948abc4c627b9a3e3519d7382b605
Sean Reilly <sean@grubwithus.com>
2012-03-06 03:00:24 -0600

    user version 1.1 so we can have some visits

--- lib/korboard/client.rb
+++ lib/korboard/client.rb
@@ -7,7 +7,9 @@ class Korboard::Client
 
   def initialize
 
+
     @token = ::Rails.application.config.korboard.api_token
+    @iteration_number = ::Rails.application.config.korboard.iteration_number
     @http = Net::HTTP.new('api.korboard.com')
     @headers = {
       'Content-Type' => 'application/json'
@@ -15,16 +17,21 @@ class Korboard::Client
   end
 
   # can do this for the rest too
-  def record_signup iteration_number,identifier,options
-    record :signup,iteration_number,identifier,options
+  def record_signup identifier,options=nil
+    record :signup,@iteration_number,identifier,options
   end
 
+  def record_visit identifier,options=nil
+    record :signup,@iteration_number,identifier,options
+  end
+
+  
   
   def record metric ,iteration_number, identifier, options =nil
     data = { :signup  => { :identifier => identifier }, :iteration => { :number => iteration_number }  }
     data.merge(options) if options
     
-    path = "/v1/#{metric.to_s.pluralize}?token=#{@token}"
+    path = "/v1.1/#{metric.to_s.pluralize}?token=#{@token}"
 
     resp, data = @http.post(path, data.to_json, @headers)
     
